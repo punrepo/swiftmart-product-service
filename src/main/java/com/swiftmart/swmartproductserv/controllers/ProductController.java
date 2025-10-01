9a083b594c9b7c9b4ce104519838504e089b6941
Puneet Pant <pmpant16@gmail.com>
2025-09-30 22:53:00 -0700

    added API to fecth all products from sandbox

--- src/main/java/com/swiftmart/swmartproductserv/controllers/ProductController.java
+++ src/main/java/com/swiftmart/swmartproductserv/controllers/ProductController.java
@@ -1,7 +1,15 @@
 package com.swiftmart.swmartproductserv.controllers;
 
+import com.swiftmart.swmartproductserv.dtos.ProductResponseDto;
+
+import com.swiftmart.swmartproductserv.models.Product;
 import com.swiftmart.swmartproductserv.services.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class ProductController {
@@ -9,6 +17,18 @@ public class ProductController {
 
     public ProductController(ProductService productService){
         this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public List<ProductResponseDto> getAllProducts()
+    {
+        List<Product> products = productService.getAllProducts();
+
+        List<ProductResponseDto> productResponseDtos =
+                products.stream().map(ProductResponseDto::from)
+                        .collect(Collectors.toList());
+
+        return productResponseDtos;
     }
 
 }
